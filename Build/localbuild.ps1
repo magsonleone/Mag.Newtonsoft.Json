2aa8b8210f214dab20a84f42d6297ed1116ef61f
Magson Leone dos Santos <magsonleone@gmail.com>
2026-05-07 15:48:06 -0700

    Use Mag package metadata in local build

--- Build/localbuild.ps1
+++ Build/localbuild.ps1
@@ -1,5 +1,9 @@
-﻿Start-Transcript ($PSScriptRoot + '\Temp\runbuild.txt')
+New-Item -Path ($PSScriptRoot + '\Temp') -ItemType Directory -Force | Out-Null
+Start-Transcript ($PSScriptRoot + '\Temp\runbuild.txt')
 
 $version = Get-Content "$PSScriptRoot\version.json" | Out-String | ConvertFrom-Json
 
-& $PSScriptRoot\runbuild.ps1 -properties @{"majorVersion"="$($version.Major).0"; "majorWithReleaseVersion"="$($version.Major).0.$($version.Release)"; "nugetPrerelease"=$version.Prerelease; "zipFileName"="Json$($version.Major)0r$($version.Release).zip"; "treatWarningsAsErrors"=$true}
+$packageVersion = "$($version.Major).0.$($version.Release)"
+$assemblyName = "Mag.Newtonsoft.Json.$packageVersion"
+
+& $PSScriptRoot\runbuild.ps1 -properties @{"majorVersion"="$($version.Major).0"; "majorWithReleaseVersion"=$packageVersion; "nugetPrerelease"=$version.Prerelease; "zipFileName"="Json$($version.Major)0r$($version.Release).zip"; "packageId"="Mag.Newtonsoft.Json"; "assemblyName"=$assemblyName; "treatWarningsAsErrors"=$true}
